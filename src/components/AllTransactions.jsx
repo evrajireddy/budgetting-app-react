62a7bfc2c5c7ec791e53e39d96b5b39382068f52
Venkata Raji Reddy Eda <evrajireddy@gmail.com>
2024-07-08 18:21:07 -0400

    completed all transactions

--- src/components/AllTransactions.jsx
+++ src/components/AllTransactions.jsx
@@ -1,22 +1,42 @@
-import { useEffect, useState } from 'react'
+// GridComponent.js
 
+import React, { useState, useEffect } from 'react';
+import { json } from 'react-router-dom';
 
-function Logs() {
+const AllTransactions = () => {
+    const [data, setData] = useState([]);
+
     useEffect(() => {
-        fetch(`http://localhost:5173/`)
-            .then(responseP => responseP.json())
-            .then(dataP => {
-                console.log("after github projects success");
-                console.log(dataP);
-                //setGithubProjects(dataP.items);
-                console.log(dataP);
-            }).catch(error => console.error('Error: fetching github project details failed:', error));
+        fetchData();
     }, []);
-    return (
-        <div >
-            All Logs
-        </div>
-    )
-}
 
-export default Logs;
+    const fetchData = async () => {
+        try {
+            const response = await fetch('http://localhost:3456/transactions'); 
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const jsonData = await response.json();
+            console.log("result" , jsonData);
+            setData(jsonData);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    return (
+        <div className="grid-container">
+            <h2>Grid Component</h2>
+            <div className="grid">
+                {data.map(item => (
+                    <div key={item.id} className="grid-item">
+                        <span>{item.title}</span>
+                        <p>{item.description}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default AllTransactions;
